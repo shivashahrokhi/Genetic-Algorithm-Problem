a5d5c60f6af43eb069718242450ed76f668ed6ba
Amir Fazlollahi <amir.h.fazlollahi@gmail.com>
2021-05-26 11:51:59 +0430

    Removing personal struggles

--- game.py
+++ game.py
@@ -11,6 +11,30 @@ class Game:
         self.current_level_index += 1
         self.current_level_len = len(self.levels[self.current_level_index])
 
+    # Get an action sequence and returns maximum steps that can be passed without lost
+    def get_steps(self,actions):
+
+        lengths = []
+        current_level = self.levels[self.current_level_index]
+        start = 0
+        end = 0
+        if current_level[0] != "_":
+            lengths.append(0)
+            start = 1
+            end = 1
+        while end != self.current_level_len - 1:
+            current_step = current_level[end]
+            if current_step == '_' or (current_step == 'G' and actions[end - 1] == '1') or (current_step == 'L' and actions[end - 1] == '2'):
+                end += 1
+            else:
+                lengths.append(end - start)
+                end += 1
+                start = end
+        if end == self.current_level_len - 1 and current_level[self.current_level_len - 1]:
+            lengths.append(end - start)
+
+        return max(lengths)
+
     # It need to change a lot
     def get_score(self, actions):
         # Get an action sequence and determine the steps taken/score
